31c82bfa759a8722a1c919c26efb0720cb159a06
Tom Chen <tc3363@drexel.edu>
2025-12-04 13:54:42 -0500

    Add header & footer

---
+++ src/lib/assets/Footer.svelte
@@ -0,0 +1,15 @@
+<footer>
+    <p>&copy; 2025 NEC Tickets Tomorrow&#46;<br>For educational use only&#46;</p>
+</footer>
+
+<style>
+    footer {
+        justify-content: flex-end;
+    }
+
+    p {
+        color: var(--white);
+        font-size: 1rem;
+        text-align: right;
+    }
+</style>
