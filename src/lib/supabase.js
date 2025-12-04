9fe2df63d3482a3b88c47f3f82d5cfd5105fa61f
Tom Chen <tc3363@drexel.edu>
2025-12-04 18:15:57 -0500

    Fix misuse of variable name

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,2 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
