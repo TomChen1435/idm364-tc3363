f38cbf644d8a20ca7fdab055aaf9e720a6bab29c
Tom Chen <tc3363@drexel.edu>
2025-12-11 07:14:40 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,8 @@
 import adapter from '@sveltejs/adapter-auto';
-import adapterNetlify from '@sveltejs/adapter-netlify';
-import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
 	kit: {
-		adapter: adapterNetlify(),
-		vite: {
-		ssr: {
-			noExternal: ['@supabase/supabase-js'] // Add this line
-		}
-		},
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
