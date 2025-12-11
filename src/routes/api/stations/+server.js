8773e8768bdd38917b12d7118b4903b8d7e7c287
Tom Chen <tc3363@drexel.edu>
2025-12-11 06:10:43 -0500

    Add APIs

---
+++ src/routes/api/stations/+server.js
@@ -0,0 +1,12 @@
+import { supabase } from '$lib/supabase';
+import { json } from '@sveltejs/kit';
+
+export async function GET() {  
+
+    const { data, error } = await supabase.from('stations').select('*')
+
+    if (error) {return json({ error: error.message }, { status: 500 });}
+
+    return json(data, { status: 200 });
+
+}
